f49cf3fce908e3127f1c7f152aa8243e68f9599b
vaibhavmr98 <vaibhavmr98@gmailcom>
2022-01-26 01:45:28 +0530

    App Component Changes For dynamic background.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 //import Todo from "./components/todoreact/todo";
  import Temp from "./components/weather/temp";
 
+
 const App = () => {
   return (
     <div className="container">
